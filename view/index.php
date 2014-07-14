82f95868caae13f0735684ba9953a6ea7bd77193
napster99 <morrisonleisure@163.com>
2014-07-15 00:16:17 +0800

    e

--- view/index.php
+++ view/index.php
@@ -12,7 +12,19 @@
 		<link rel="stylesheet" type="text/css" href="../public/css/common.css">
 	</head>
 	<body>
-		
+		<div class="wrap">
+			<div class="header clearfix">
+				<a href="http://www.jmfan.cn"><h1>宅“鸡”送--黄焖鸡米饭</h1></a>
+				<div class="areas">
+					<span class="tip-title">营业时间：</span><span class="time">09:00~21:00</span>
+					<span class="tip-title">订餐热线：</span><span class="tel">13958111541</span>
+					<span>|</span><span class="tel ml10">13965774326</span>
+				</div>
+			</div>
+			<div class="banner">
+				<img src="../public/images/pic.jpg" width="1000">
+			</div>
+		</div>
 	</body>
 	<script type="text/javascript" src="../public/js/jquery-1.11.1.js"></script>
 	<script type="text/javascript" src="../public/js/index.js"></script>
