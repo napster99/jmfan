8fa75d79f5922a3ddb9d60fcab9355c05c8bc7a0
napster99 <morrisonleisure@163.com>
2014-07-15 16:59:07 +0800

    static web css

--- view/index.php
+++ view/index.php
@@ -10,13 +10,14 @@
 		<meta name="Description" content="味达外卖,味捷外卖，">
 		<link rel="stylesheet" type="text/css" href="../public/css/reset.css">
 		<link rel="stylesheet" type="text/css" href="../public/css/common.css">
+		<link rel="stylesheet" type="text/css" href="../public/font/iconfont.css">
 	</head>
 	<body>
 		<div class="wrap">
 			<div class="header clearfix">
 				<a href="http://www.jmfan.cn"><h1>宅“鸡”送--黄焖鸡米饭</h1></a>
 				<div class="areas">
-					<span class="tip-title">营业时间：</span><span class="time">09:00~21:00</span>
+					<span class="tip-title">营业时间：</span><span class="time">09:00--21:00</span>
 					<span class="tip-title">订餐热线：</span><span class="tel">13958111541</span>
 					<span>|</span><span class="tel ml10">13965774326</span>
 				</div>
@@ -24,6 +25,240 @@
 			<div class="banner">
 				<img src="../public/images/pic.jpg" width="1000">
 			</div>
+			<div class="outer order-process">
+				<i class="icon iconfont">&#xf01ea</i>
+				<span class="text">确定地址</span>
+				<i class="icon iconfont arrow">&#xf0114;</i>
+
+				<i class="icon iconfont">&#xf01be</i>
+				<span class="text">订购餐品</span>
+				<i class="icon iconfont arrow">&#xf0114;</i>
+
+				<i class="icon iconfont">&#xf006e</i>
+				<span class="text">选择支付</span>
+				<i class="icon iconfont arrow">&#xf0114;</i>
+
+				<i class="icon iconfont">&#xf00eb</i>
+				<span class="text">提交订单</span>
+				<i class="icon iconfont arrow">&#xf0114;</i>
+
+				<i class="icon iconfont">&#xf0090</i>
+				<span class="text">等待送达</span>
+			</div>
+			<div class="cut-off-line"></div>
+			<!-- S 确定地址 -->
+			<div class="outer module">
+				<span class="block-title"><i class="icon iconfont">&#xf01ea</i>确定地址</span>
+				<ul class="address clearfix">
+					<li class="item"><i class="icon iconfont">&#xf0156</i><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+					<li class="item"><a href="javascript:;">xxxxx</a></li>
+				</ul>
+				<div class="line"></div>
+				<div class="other-contact">
+					<span class="text-filed"><i class="icon iconfont">&#xf00a2</i>我的联系方式</span><input class="text-input" placeholder="必填" />
+					<span class="text-filed" style="margin-left:10px;"><i class="icon iconfont">&#xf01ea</i>我的具体地址</span><input class="text-input" style="width:232px;" placeholder="必填" />
+					<span class="text-filed" style="margin-left:10px;"><i class="icon iconfont">&#xf00c4</i>送达时间</span><input class="text-input" placeholder="可选"/>
+				</div>
+				<div class="line"></div>
+				<div class="buttons">
+					<button class="butn butn-orange">确定</button>
+					<button class="butn butn-cancel">重置</button>
+				</div>
+			</div>
+			<!-- E 确定地址 -->
+
+			<!-- S 订购餐品 -->
+			<div class="outer module clearfix">
+				<span class="block-title"><i class="icon iconfont">&#xf01be</i>订购餐品</span>
+				<div class="clearfix" onselectstart="return false;">
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份不辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份微辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份中辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份重辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+				</div>
+				<div class="clearfix" onselectstart="return false;">
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>20.00</span></dd>
+						<dd class="nap-text">大份不辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>20.00</span></dd>
+						<dd class="nap-text">大份微辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>20.00</span></dd>
+						<dd class="nap-text">大份中辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>20.00</span></dd>
+						<dd class="nap-text">大份重辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+				</div>
+				<div class="line"></div>
+				<div class="buttons">
+					<button class="butn butn-orange">确定</button>
+					<button class="butn butn-cancel">重置</button>
+				</div>
+			</div>
+			<!-- E 订购餐品 -->
+
+			<!-- S 订购饮料 -->
+			<div class="outer module">
+				<span class="block-title"><i class="icon iconfont">&#xf0089</i>订购饮料</span>
+				<div class="clearfix" onselectstart="return false;">
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份不辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份微辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份中辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+					<dl class="jmfan">
+						<dt><img class="jmfanpic" src="../public/images/jmfan.png"></dt>
+						<dd class="tb-rmb-num"><span class="tb-rmb">¥</span><span>15.00</span></dd>
+						<dd class="nap-text">小份重辣</dd>
+						<dd>
+							<span class="nap-text">数量</span>
+							<span class="tb-stock">
+	            	<i class="icon iconfont">&#xf0153</i>
+	            	<input type="text" class="tb-text" value="0" title="请输入购买量">
+	            	<i class="icon iconfont">&#xf0154</i>
+	        		</span>
+	        	</dd>
+					</dl>
+			</div>
+			<!-- E 订购饮料 -->
+
 		</div>
 	</body>
 	<script type="text/javascript" src="../public/js/jquery-1.11.1.js"></script>
